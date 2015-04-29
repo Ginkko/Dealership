4c478a8e2483c84e4336753de12740e447503b42
Toby Alden & Alec Arme <student@epicodus.com>
2015-04-29 09:31:27 -0700

    Add spec #save. Passes spec. Adds #clear loop for testing.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,8 +1,24 @@
 class Vehicle
   @@vehicles = []
 
+  define_method(:initialize) do |make, model, year|
+    @make = make
+    @model = model
+    @year = year
+  end
+
+
   define_singleton_method(:all) do
     @@vehicles
+  end
+
+
+  define_singleton_method(:clear) do
+    @@vehicles = []
+  end
+
+  define_method(:save) do
+    @@vehicles.push(self)
   end
 
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -3,11 +3,27 @@ require('vehicle')
 require('pry')
 
 describe('Vehicle') do
+  before() do
+    Vehicle.clear()
+  end
 
   describe('.all') do
     it('returns the contents of vehicle list') do
       expect(Vehicle.all()).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it('adds a vehicle to the vehicle list') do
+      test_vehicle = Vehicle.new('Toyota', 'Prius', 2000)
+      test_vehicle.save()
+      expect(Vehicle.all()).to(eq([test_vehicle]))
+    end
+
+
+
+  end
+
+
 
 end
